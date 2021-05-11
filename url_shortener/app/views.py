95226fbd72f672aa1603c7835f9da84063f2bb2b
James Wheadon <james.wheadon@yahoo.com>
2021-05-11 17:23:35 +0100

    random short url

--- url_shortener/app/views.py
+++ url_shortener/app/views.py
@@ -3,14 +3,18 @@ from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import GenerateUrlForm
 from .models import Url
+import random
+import string
 
 def index(req):
     if req.method == 'POST':
         form = GenerateUrlForm(req.POST)
         if form.is_valid() :
             url = form.save()
-            shortUrl = "hello.com"
-            result = {'result': shortUrl}
+            random_string = ''.join(random.choice(string.ascii_lowercase) for i in range(5))
+            url.short_url = random_string
+            url.save()
+            result = {'result': random_string}
             return render (req, 'app/newurl.html', result)
 
     else :
@@ -18,4 +22,7 @@ def index(req):
         return render(req, 'app/index.html', data)
 
 def show(req, id):
-    return HttpResponse(f'<h3>Your short url doesn\'t exist yet!</h3>')
+    print(id, type(id))
+    url = Url.objects.get(short_url=id)
+    print(url)
+    return HttpResponse(f'<h3>Url: {url.original}</h3>')
