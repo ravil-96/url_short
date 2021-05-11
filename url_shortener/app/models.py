d4db38a71909c5424fde475c69ecfbc4e1606450
Ravil Shafiyev <ravil-96@mail.ru>
2021-05-11 15:25:23 +0100

    make model for url and migrations

--- url_shortener/app/models.py
+++ url_shortener/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Url(models.Model):
+    original = models.TextField()
+    short_url = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f'Here is your short url: {self.short_url}.\n (Generated from original: {self.original}'
+
+
+    
--- url_shortener/app/views.py
+++ url_shortener/app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render
 
-# Create your views here.
+from .models import Url
+
+def index(req):
+    return HttpResponse("<h3>Here is your first Django app page!</h3>")
+
+def show(req, id):
+    return HttpResponse(f'<h3>Your short url doesn\'t exist yet!</h3>')
