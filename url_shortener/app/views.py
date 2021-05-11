558db38f7a8cb1223076d95c7a75f0135c3bcc32
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-11 16:49:59 +0100

    :zap: on submit of url form, show result - template made

--- url_shortener/app/views.py
+++ url_shortener/app/views.py
@@ -5,8 +5,18 @@ from .forms import GenerateUrlForm
 from .models import Url
 
 def index(req):
-    data = GenerateUrlForm
-    return render(req, 'app/index.html', data)
+    if req.method == 'POST':
+        form = GenerateUrlForm(req.POST)
+        if form.is_valid() :
+            url = form.save()
+            shortUrl = "hello.com"
+            result = {'result': shortUrl}
+            return render (req, 'app/newurl.html', result)
+
+
+    else :
+        data = {'form': GenerateUrlForm}
+        return render(req, 'app/index.html', data)
 
 def show(req, id):
     return HttpResponse(f'<h3>Your short url doesn\'t exist yet!</h3>')
