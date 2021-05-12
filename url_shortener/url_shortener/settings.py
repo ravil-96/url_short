ce1fb01e6b0a6d53ce81a3e9cd0beb20f9fd5ff2
Ravil Shafiyev <ravil-96@mail.ru>
2021-05-12 09:09:51 +0100

    add django-heroku to settings

--- url_shortener/url_shortener/settings.py
+++ url_shortener/url_shortener/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,4 +125,5 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+import django_heroku
 django_heroku.settings(locals())
