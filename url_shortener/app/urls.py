e1d7aa47b6c14bbb1acb25adb12ae999aa7f248d
Ravil Shafiyev <ravil-96@mail.ru>
2021-05-12 10:11:00 +0100

    remove / from url

--- url_shortener/app/urls.py
+++ url_shortener/app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.index, name='app-index'),
+    path('', views.index, name='app-index'),
     path('<str:id>/', views.show, name='app-show')
 ]
