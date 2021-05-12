f67e13d5deaecbb794ee423cbd4a60971a97f52f
Ravil Shafiyev <ravil-96@mail.ru>
2021-05-12 09:41:26 +0100

    add settings

--- url_shortener/url_shortener/settings.py
+++ url_shortener/url_shortener/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Extra places for collectstatic to find static files.
